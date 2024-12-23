d39ec8f94ec6a2fea63b70097990d14e14522e25
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2024-12-23 13:40:58 +0800

    Time: 3 ms (28.69%), Space: 9.9 MB (24.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    char alphabetToLower(char ch){
-        if (ch <= 'Z' && ch >= 'A'){
-            return (ch-'A'+'a');
-        }
-
-        return ch;
-    }
-    bool isAlphaberOrNum(char ch){
-        if ((ch <= 'z' && ch >= 'a')||
-            (ch <= '9' && ch >= '0')||
-            (ch <= 'Z' && ch >= 'A')){
-            return true;
-        }
-        return false;
-    }
     bool isPalindrome(string s) {
         int left = 0;
         int right = s.size()-1;
 
         while(left<right){
-            while (left<right && !isAlphaberOrNum(s[left])) left++;
-            while (left<right && !isAlphaberOrNum(s[right])) right--;
+            while (left<right && !std::isalnum(s[left])) left++;
+            while (left<right && !std::isalnum(s[right])) right--;
 
-            if (alphabetToLower(s[left])!=alphabetToLower(s[right])){
+            if (std::tolower(s[left])!=std::tolower(s[right])){
                 return false;
             }
             left++;
