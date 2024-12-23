6bf9e7521a13c4ff540cd1bf93ae3b88dc78a1dd
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2024-12-23 16:45:51 +0800

    Time: 844 ms (5.01%), Space: 290.9 MB (5.01%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    static bool compareMap(std::unordered_map<char, int> map_s,
+                           std::unordered_map<char, int> map_p){
+        for (auto it:map_p){
+            if (!map_s.count(it.first)||
+                map_s[it.first]!=it.second){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<int> findAnagrams(string s, string p) {
+        int size_p = p.size();
+        int size_s = s.size();
+        std::vector<int> res;
+
+        if (size_s < size_p)
+            return res;
+
+        std::unordered_map<char, int> map_p;
+        std::unordered_map<char, int> map_s;
+        for(char ch:p){
+            map_p[ch]++;
+        }
+
+        int head = 0;
+        int size = size_s - size_p;
+
+        while(head <= size){
+            if (head == 0){
+                for(int i=0; i<size_p; i++){
+                    map_s[s[i]]++;
+                }
+            }
+            else{
+                map_s[s[head-1]]--;
+                map_s[s[head+size_p-1]]++;
+            }
+            if (compareMap(map_s, map_p)){
+                res.push_back(head);
+            }
+
+            head++;
+        }
+
+        return res;
+    }
+};
