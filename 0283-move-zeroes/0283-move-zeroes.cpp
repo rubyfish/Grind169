38aeb1e526cb6df1f294a3cf50448801a108b8e2
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-01-03 23:27:21 +0800

    Time: 0 ms (100%), Space: 23.7 MB (51.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int p1 = 0;
-        int p2 = 0;
+        int index_nonzero = 0;
+        int N = nums.size();
 
-        while (p2 < nums.size() && p1 < nums.size()) {
-            if (nums[p2] == 0) {
-                p2++;
-            } 
-            else if (nums[p1] != 0) {
-                p1++;
-            } 
-            else if (p1 > p2) {
-                p2++;
-            } 
-            else {
-                std::swap(nums[p1], nums[p2]);
-                p1++;
-                p2++;
+        for (int i = 0; i < N; i++){
+            if (nums[i] != 0){
+                nums[index_nonzero++] = nums[i];
             }
+        }
+
+        for(int i = index_nonzero; i < N; i++){
+            nums[i] = 0;
         }
     }
 };
