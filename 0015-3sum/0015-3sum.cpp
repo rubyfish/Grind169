406df87f77aa8bbfb11e97f739833170633d81c2
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-03-31 19:04:56 +0800

    Time: 2151 ms (5%), Space: 473 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    void twoSum(vector<int>& nums, int i, vector<vector<int>>& results){
+    vector<vector<int>> twoSum(vector<int>&nums, int start_index, int target){
         std::unordered_set<int> set;
-        for (int j = i+1; j<nums.size(); j++){
-            int target = -nums[i]-nums[j];
+        vector<vector<int>> results;
 
-            if (set.count(target)){
-                results.push_back({nums[i],nums[j],target});
+        for(int i = start_index; i < nums.size(); i++){
+            int tmp_target = target - nums[i];
+            if (set.count(tmp_target)){
+                results.push_back({tmp_target, nums[i]});
 
-                while (j+1<nums.size() && nums[j] == nums[j+1]){
-                    j++;
+                while(i+1 < nums.size() && nums[i+1] == nums[i]){
+                    i++;
                 }
             }
-
-            set.insert(nums[j]);
-        }
-    }
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        std::vector<std::vector<int>> results;
-        std::sort(nums.begin(), nums.end());
-
-        for(int i = 0; i<nums.size(); i++){
-            //since the array has been sorted, we can't find threesum equal to zero if first number is larger than zero
-            if (nums[i]>0)
-                break;
-            if (i==0 || nums[i]!=nums[i-1]){
-                twoSum(nums, i, results);
-            }
+            set.insert(nums[i]);
         }
 
         return results;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> results;
+        std::sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); i++){
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            int target = -nums[i];
+            vector<vector<int>> twosum_res = twoSum(nums, i + 1, target);
+
+            for (vector<int> tmp_vec:twosum_res){
+                results.push_back({nums[i], tmp_vec[0], tmp_vec[1]});
+            }
+        }
+
+        return results;    
     }
 };
