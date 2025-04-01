d184ee1c9c970d407404de9bc0f567775a4f5aa2
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-01 18:35:18 +0800

    Time: 0 ms (100%), Space: 12.1 MB (99.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1 = m-1;
-        int p2 = n-1;
-        int cur_p = m+n-1;
+        int p0 = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
 
-        while(p2 >= 0){
-            if (p1 >= 0 && nums1[p1] > nums2[p2]){
-                nums1[cur_p] = nums1[p1];
-                p1--;
-            }
-            else{
-                nums1[cur_p] = nums2[p2];
+        while (p0 >= 0){
+            int tmp = 0;
+            if (p1 < 0 || (p2 >= 0 && nums1[p1] < nums2[p2])){
+                tmp = nums2[p2];
                 p2--;
             }
-            cur_p--;
+            else {
+                tmp = nums1[p1];
+                p1--;
+            }
+            nums1[p0] = tmp;
+            p0--;
         }
     }
 };
