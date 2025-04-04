bd34396b9c8769e1857b3b9c198f3cf8248ac63f
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-04 12:34:28 +0800

    Time: 0 ms (100%), Space: 17.8 MB (57.81%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int result = nums[0];
-        int max_num = nums[0];
-        int min_num = nums[0];
+        int pre_min = nums[0];
+        int pre_max = nums[0];
 
-        for(int i = 1; i<nums.size(); i++){
+        for (int i = 1; i < nums.size(); i++){
             int cur = nums[i];
-            int tmp_max = std::max(cur*max_num, cur*min_num);
-            int tmp_min = std::min(cur*max_num, cur*min_num);
-
-            max_num = std::max(cur, tmp_max);
-            min_num = std::min(cur, tmp_min);
-
-            result = std::max(result, max_num);
+            int cur_min = std::min(cur, std::min(cur*pre_min, cur*pre_max));
+            int cur_max = std::max(cur, std::max(cur*pre_min, cur*pre_max));
+            result = std::max(result, cur_max);
+            pre_min = cur_min;
+            pre_max = cur_max;
         }
 
         return result;
