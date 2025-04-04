0dc1140584ab74749660fbaeee2135b32f152ded
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-04 12:39:36 +0800

    Time: 0 ms (100%), Space: 63 MB (48.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0;
-        int right = height.size()-1;
+        int right = height.size() - 1;
         int area = 0;
 
-        while(left<right){
-            int tmp_area = (right-left)*std::min(height[left], height[right]);
+        while(left < right){
+            int tmp_area = std::min(height[left], height[right]) * (right - left);
             area = std::max(area, tmp_area);
-            if (height[left] < height[right])
-                left++;
-            else
+
+            if (height[left] > height[right])
                 right--;
+            else
+                left++;
         }
 
         return area;
