c18be1e9218e041d33bd17bcd73db09042b83d23
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-05 22:53:54 +0800

    Time: 29 ms (45.35%), Space: 20.8 MB (38.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -17,6 +17,25 @@ public:
         DFS(grid, i, j - 1);
     }
 
+    std::queue<pair<int, int>> neighbors;
+    void BFS(vector<vector<char>>& grid){
+        while(!this->neighbors.empty()){
+            pair<int, int> n = neighbors.front();
+            neighbors.pop();
+            int i = n.first;
+            int j = n.second;
+            if (i < 0 || i >= this->m || j < 0 || j >= this->n)
+                continue;
+            if (grid[i][j] != this->land)
+                continue;
+            grid[i][j] = this->water;
+            neighbors.push({i+1, j});
+            neighbors.push({i-1, j});
+            neighbors.push({i, j+1});
+            neighbors.push({i, j-1});
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         this->m = grid.size();
         this->n = grid[0].size();
@@ -26,7 +45,13 @@ public:
             for (int j = 0; j < this->n; j++){
                 if (grid[i][j] == this->land){
                     count++;
-                    DFS(grid, i, j);
+                    
+                    //DFS
+                    //DFS(grid, i, j);
+                    
+                    //BFS
+                    this->neighbors.push({i, j});
+                    BFS(grid);
                 }
             }
         }
