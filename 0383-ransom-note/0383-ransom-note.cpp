d55e847934164a11f3ed128707bc920402bfb4a5
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-08 13:05:15 +0800

    Time: 5 ms (40.29%), Space: 12.1 MB (10.45%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,17 +3,19 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         if (ransomNote.size() > magazine.size())
             return false;
-        
-        std::unordered_map<char, int> map;
-        for(char ch:magazine){
-            map[ch]++;
+
+        std::unordered_map<char, int> mappings;
+
+        for (char ch:magazine){
+            mappings[ch]++;
         }
-        
-        for(char ch:ransomNote){
-            if(!map.count(ch)||map[ch]==0)
+
+        for (char ch:ransomNote){
+            mappings[ch]--;
+            if (mappings[ch] < 0)
                 return false;
-            map[ch]--;
         }
+
         return true;
     }
 };
