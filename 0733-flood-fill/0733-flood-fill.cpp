d9f69c602a44c47e402c3a0c72fe9e838b89b0e9
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-08 18:30:55 +0800

    Time: 0 ms (100%), Space: 18 MB (89.45%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int target, int color){
+    void dfs(vector<vector<int>>& image, int sr, int sc, int src_color, int target_color){
         if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size())
             return;
-        if (image[sr][sc] != target)
+        if (image[sr][sc] != src_color)
             return;
-        
-        image[sr][sc] = color;
-        dfs(image, sr+1, sc, target, color);
-        dfs(image, sr-1, sc, target, color);
-        dfs(image, sr, sc+1, target, color);
-        dfs(image, sr, sc-1, target, color);
+        image[sr][sc] = target_color;
+
+        dfs(image, sr + 1, sc, src_color, target_color);
+        dfs(image, sr - 1, sc, src_color, target_color);
+        dfs(image, sr, sc + 1, src_color, target_color);
+        dfs(image, sr, sc - 1, src_color, target_color);
     }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image[sr][sc] != color)
-            dfs(image, sr, sc, image[sr][sc], color);
+        if (image[sr][sc] == color)
+            return image;
+        dfs(image, sr, sc, image[sr][sc], color);
 
         return image;
     }
