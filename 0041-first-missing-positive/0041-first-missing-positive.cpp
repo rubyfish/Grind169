96c1009d807981ee4f4d856e8def03f146255415
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-08 18:19:55 +0800

    Time: 3 ms (55.09%), Space: 55 MB (74.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int min_positive = INT_MAX;
+        //use input vector as hashmap
+        //convert negative and number>nums.size() to 1
+        //iterative all value, if value x exists, turn index-1 to negative
+        //iterative all value, if current value is positive, return its index+1
+        int n_size = nums.size();
         bool has_one = false;
-
-        for(int i = 0; i < nums.size(); i++){
-            if (nums[i] <= 0){
+        for (int i = 0; i < n_size; i++){
+            if (nums[i] <= 0 || nums[i] > n_size){
                 nums[i] = 1;
             }
             else if (nums[i] == 1){
                 has_one = true;
             }
         }
-
-        for (int i = 0; i < nums.size(); i++){
-            bool flag = false;
-            int target_index = abs(nums[i])-1;
-            if (target_index < nums.size()){
-                if (target_index == 0 && has_one){
-                    flag = true;
-                }
-                else if (target_index != 0){
-                    flag = true;
-                }
-            }
-            if (flag){
-                if (nums[target_index] > 0){
-                    nums[target_index]*=-1;
-                }
-            }
+        
+        for (int i = 0; i < n_size; i++){
+            //remember to turn current number to positive
+            int curr_n = abs(nums[i]);
+            if (curr_n == 1 && has_one == false)
+                continue;
+               
+            if (nums[curr_n - 1] > 0)
+                nums[curr_n - 1] *= -1;
         }
-
-        for (int i = 0; i < nums.size(); i++){
+        
+        for (int i = 0; i < n_size; i++){
             if (nums[i] > 0){
-                return i+1;
+                return i + 1;
             }
         }
-
-        return nums.size()+1;
+        
+        return n_size + 1;
     }
 };
