8dfa6d4918493981bd6f1bb3f3a5cddc39a0dd3f
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-04-10 18:26:58 +0800

    Time: 0 ms (100%), Space: 15.3 MB (33.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,18 +4,21 @@ public:
         int left = 0;
         int right = nums.size() - 1;
 
-        while (right >= left) {
-            int mid = (right + left)/2;
-            if (nums[mid] == target)
+        while (left <= right){
+            int mid = (right - left)/2 + left;
+            int n = nums[mid];
+
+            if (n == target)
                 return mid;
-            else if (nums[mid] >= nums[left]) {
-                if (nums[left] <= target && target < nums[mid])
+            else if (nums[left] <= n){
+                if (nums[left] <= target && target < n){
                     right = mid - 1;
+                }
                 else
                     left = mid + 1;
             }
-            else {
-                if (nums[right] >= target && target > nums[mid])
+            else{
+                if (n < target && target <= nums[right])
                     left = mid + 1;
                 else
                     right = mid - 1;
