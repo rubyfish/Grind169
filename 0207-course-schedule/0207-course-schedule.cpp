c09417381ef61092a4057d33d10b8eace1d590e9
ruby_ju <34569240+rubyfish@users.noreply.github.com>
2025-05-14 12:21:51 +0800

    Time: 7 ms (51.77%), Space: 20.3 MB (26.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses);
         vector<vector<int>> relations(numCourses);
-        vector<int> order;
+        vector<int> indegree(numCourses);
+        vector<int> courseOrder;
         queue<int> q;
 
-        for (vector<int> prerequisite:prerequisites){
+        for(vector<int> prerequisite:prerequisites){
             relations[prerequisite[1]].push_back(prerequisite[0]);
             indegree[prerequisite[0]]++;
         }
@@ -17,17 +17,17 @@ public:
         }
 
         while (!q.empty()){
-            int cur_course = q.front();
+            int curCourse = q.front();
             q.pop();
-            order.push_back(cur_course);
+            courseOrder.push_back(curCourse);
 
-            for (int course:relations[cur_course]){
+            for (int course:relations[curCourse]){
                 indegree[course]--;
                 if (indegree[course] == 0)
                     q.push(course);
             }
         }
 
-        return order.size() == numCourses ? true:false;
+        return courseOrder.size() == numCourses;
     }
 };
